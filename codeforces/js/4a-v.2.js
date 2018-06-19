e769c5ffc5bc0fceace4b3630658e5ce6a838e89
rbenjani <benjani86@gmail.com>
2018-06-19 23:49:55 +0200

    Added other solutions to 4A (JS - Codeforces)

---
+++ codeforces/js/4a-v.2.js
@@ -0,0 +1,5 @@
+/**
+ * Version 2
+ */
+var w = readline();
+print(w < 4 || w % 2 ? 'NO' : 'YES');
---
+++ codeforces/js/4a-v.3.js
@@ -0,0 +1,8 @@
+/**
+ * Version 3 (more faster than the others) 62ms vs 92ms
+ */
+var a = readline();
+if (a % 2 == 0 && a > 2)
+    print('YES');
+else 
+    print('NO');
